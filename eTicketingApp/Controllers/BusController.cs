6d8556a31938af0fcef3af18d885d856d4ecda73
username-unavailable-is-not-available <19-39872-1@student.aiub.edu>
2023-08-26 16:43:51 +0600

    Added vehicle and bus Controllers.

---
+++ eTicketingApp/Controllers/BusController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http;
+
+namespace eTicketApp.Controllers
+{
+    public class BusController : ApiController
+    {
+
+    }
+}
---
+++ eTicketingApp/Controllers/VehicleController.cs
@@ -0,0 +1,26 @@
+﻿using BLL.Services;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace eTicketApp.Controllers
+{
+    public class VehicleController : ApiController
+    {
+        [HttpGet]
+        [Route("api/vehicle/all")]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                var data = VehicleService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}

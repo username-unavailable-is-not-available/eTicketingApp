1313c6b90e82626363189cd8e4702f8f5e90dac9
username-unavailable-is-not-available <19-39872-1@student.aiub.edu>
2023-08-26 16:41:02 +0600

    Added DataAccessFactory.

---
+++ DAL/DataAccessFactory.cs
@@ -0,0 +1,18 @@
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using DAL.Repos;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IRepo<Vehicle, int, bool> VehicleData()
+        {
+            return new VehicleRepo();
+        }
+        public static IRepo<Bus, int, bool> BusData()
+        {
+            return new BusRepo();
+        }
+    }
+}

a0e003e1859556d8506982f26da0e983c4891cb3
username-unavailable-is-not-available <19-39872-1@student.aiub.edu>
2023-08-26 16:31:28 +0600

    Minor fixes.

--- DAL/Repos/BusRepo.cs
+++ DAL/Repos/BusRepo.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL.Repos
 {
-    internal class BusRepo
+    public class BusRepo : Repo, IRepo<Bus, int, bool>
     {
+        public bool Create(Bus obj)
+        {
+            db.Buses.Add(obj);
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            var exobj = Get(id);
+            db.Buses.Remove(exobj);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<Bus> Get()
+        {
+            return db.Buses.ToList();
+        }
+
+        public Bus Get(int id)
+        {
+            return db.Buses.Find(id);
+        }
+
+        public bool Update(Bus obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DAL/Repos/VehicleRepo.cs
+++ DAL/Repos/VehicleRepo.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL.Repos
 {
-    internal class VehicleRepo
+    public class VehicleRepo : Repo, IRepo<Vehicle, int, bool>
     {
+        public bool Create(Vehicle obj)
+        {
+            db.Vehicles.Add(obj);
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Vehicle> Get()
+        {
+            return db.Vehicles.ToList();
+        }
+
+        public Vehicle Get(int id)
+        {
+            return db.Vehicles.Find(id);
+        }
+
+        public bool Update(Vehicle obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

f7535708740ac99c78624e049457984c24edf098
username-unavailable-is-not-available <19-39872-1@student.aiub.edu>
2023-08-26 16:34:08 +0600

    Added vehicle and bus DTOs.

---
+++ BLL/DTOs/BusDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BLL.DTOs
+{
+    public class BusDTO
+    {
+        public int Id { get; set; }
+        public string BusName { get; set; }
+        public string Route { get; set; }
+        public decimal Price { get; set; }
+        public int VehicleId { get; set; }
+    }
+}
---
+++ BLL/DTOs/VehicleDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BLL.DTOs
+{
+    public class VehicleDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

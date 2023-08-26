5fa1ab71478fd0444a8bd175fe093c9a4174e014
username-unavailable-is-not-available <19-39872-1@student.aiub.edu>
2023-08-26 16:36:47 +0600

    Added vehicle and bus Services.

---
+++ BLL/Services/BusService.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BLL.Services
+{
+    public class BusService
+    {
+        public static List<BusDTO> Get()
+        {
+            var data = DataAccessFactory.BusData().Get();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Bus, BusDTO>();
+            });
+            var mapper = new Mapper(config);
+            var converted = mapper.Map<List<BusDTO>>(data);
+            return converted;
+        }
+
+        public static List<BusDTO> Get(string route)
+        {
+            var data = (from b in DataAccessFactory.BusData().Get()
+                        where b.Route.ToLower().Contains(route.ToLower())
+                        select b).ToList();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Bus, BusDTO>();
+            });
+            var mapper = new Mapper(config);
+            var converted = mapper.Map<List<BusDTO>>(data);
+            return converted;
+        }
+
+        public static List<BusDTO> Get(decimal price)
+        {
+            var data = (from b in DataAccessFactory.BusData().Get()
+                        where b.Price == price
+                        select b).ToList();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Bus, BusDTO>();
+            });
+            var mapper = new Mapper(config);
+            var converted = mapper.Map<List<BusDTO>>(data);
+            return converted;
+        }
+
+        public static List<BusDTO> Get(string route, decimal price)
+        {
+            var data = (from b in DataAccessFactory.BusData().Get()
+                        where b.Route.ToLower().Contains(route.ToLower())
+                            && b.Price == price
+                        select b).ToList();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Bus, BusDTO>();
+            });
+            var mapper = new Mapper(config);
+            var converted = mapper.Map<List<BusDTO>>(data);
+            return converted;
+        }
+    }
+}
---
+++ BLL/Services/VehicleService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF.Models;
+using System.Collections.Generic;
+
+namespace BLL.Services
+{
+    public class VehicleService
+    {
+        public static List<VehicleDTO> Get()
+        {
+            var data = DataAccessFactory.VehicleData().Get();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Vehicle, VehicleDTO>();
+            });
+            var mapper = new Mapper(config);
+            var converted = mapper.Map<List<VehicleDTO>>(data);
+            return converted;
+        }
+
+        public static VehicleDTO Get(int id)
+        {
+            var data = DataAccessFactory.VehicleData().Get(id);
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Vehicle, VehicleDTO>();
+            });
+            var mapper = new Mapper(config);
+            var converted = mapper.Map<VehicleDTO>(data);
+            return converted;
+        }
+    }
+}
